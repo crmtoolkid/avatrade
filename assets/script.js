b772bc68a04f31c36af484024e1ca6c696787859
crmtoolkid <tech@triplew.in>
2026-04-21 16:23:14 +0530

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+const navToggle = document.getElementById('navToggle');
+const mainNav = document.getElementById('mainNav');
+
+if (navToggle && mainNav) {
+  navToggle.addEventListener('click', () => {
+    mainNav.classList.toggle('open');
+  });
+
+  mainNav.querySelectorAll('a').forEach(link => {
+    link.addEventListener('click', () => {
+      mainNav.classList.remove('open');
+    });
+  });
+}
